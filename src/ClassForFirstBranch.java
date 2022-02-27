6d1e999368bace94bf3a67f5205d87a572783376
Ilya Sayfetdinov <sayfetdinov1408@yandex.ru>
2022-02-27 16:33:30 +0500

    Удаление

--- src/ClassForFirstBranch.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Ilya on 2022-02-27 16:27
- **/
-
-public class ClassForFirstBranch {
-}
--- src/Git.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Ilya on 2022-02-27 16:07
- **/
-
-public class Git {
-}
