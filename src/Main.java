90d093d9e243c41e9f6ab2c4a78cf07542b71062
Ilya Sayfetdinov <sayfetdinov1408@yandex.ru>
2022-03-07 00:20:28 +0500

    first

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, это master ветка");
         Main main = new Main();
-        main.write("собеседование");
+        main.write("КОНФЛИКТ ВЕРСИЙ");
     }
 
     public void write(String message) {
         System.out.println("Напишу-ка я слово: " + message);
+        System.out.println("Java the best");
     }
 }
