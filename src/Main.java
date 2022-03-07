4dec535f9f0cc2a705d0e1a30789d025215852c2
Ilya Sayfetdinov <sayfetdinov1408@yandex.ru>
2022-03-07 18:07:13 +0500

    dsfhsdf

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, это master ветка");
         Main main = new Main();
-        main.write("собеседование");
-        main.write("КОНФЛИКТ ВЕРСИЙ");
+        main.write("главная ветка");
     }
 
     public void write(String message) {
